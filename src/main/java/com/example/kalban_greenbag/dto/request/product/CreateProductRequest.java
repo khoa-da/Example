842505cfc3849e7f9447ed246633f6af4413a8d8
DO ANH KHOA <95236523+khoa-da@users.noreply.github.com>
2024-09-26 21:06:49 +0700

    Update CreateProductRequest.java

--- src/main/java/com/example/kalban_greenbag/dto/request/product/CreateProductRequest.java
+++ src/main/java/com/example/kalban_greenbag/dto/request/product/CreateProductRequest.java
@@ -18,7 +18,6 @@ public class CreateProductRequest {
     @NotNull(message = "ProductName is required")
     private String productName;
     @NotNull(message = "Stock is required")
-    @Size(min = 1, max = 100000, message = "Stock must be between 1 and 100")
     private Integer stock;
     private BigDecimal finalPrice;
     private String status;
